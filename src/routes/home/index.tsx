6723714f5306519d51a2ed8ecbd5f0f8bdb63f1e
heisdezz <desto4q@gmail.com>
2026-07-08 13:49:34 +0100

    6 to 3

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -133,7 +133,7 @@ function RouteComponent() {
             standards.
           </p>
 
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 ">
             {accreditations.map((item) => (
               <div
                 key={item.name}
