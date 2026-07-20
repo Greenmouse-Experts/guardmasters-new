33156717418977c0978b748ab8810d42a436e79a
heisdezz <desto4q@gmail.com>
2026-07-20 10:27:25 +0100

    accreditations fonts

--- src/routes/home/programs/-components/ProgramCard.tsx
+++ src/routes/home/programs/-components/ProgramCard.tsx
@@ -62,7 +62,7 @@ export default function ProgramCard({
           {course.title}
         </h3>
 
-        <p className="mb-6 line-clamp-3  leading-relaxed text-accent-content/60">
+        <p className="mb-6 line-clamp-3  leading-relaxed ">
           {course.shortDesc}
         </p>
 
