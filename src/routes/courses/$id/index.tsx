58f974ec64fb55fa7cd848c7de9ecbbdaf23efad
heisdezz <desto4q@gmail.com>
2026-08-04 11:25:16 +0100

    about us changes,blog page and routing changes

--- src/routes/courses/$id/index.tsx
+++ src/routes/courses/$id/index.tsx
@@ -97,7 +97,9 @@ function RouteComponent() {
                 // badge="Outcomes"
                 title={
                   <>
-                    Program Objectives{" "}
+                    <div className="text-lg  text-accent uppercase  w-fit  bg-white py-2 ring-current/40 px-7 rounded-full  ring self-start shadow font-bold">
+                      Program Objectives{" "}
+                    </div>{" "}
                     <em className="text-primary italic">
                       {" "}
                       & Learning Outcomes.
